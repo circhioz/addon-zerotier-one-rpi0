ff26e2ea2d3b1993d2ee0d04c5f8fced1c5e08f1
Francesco Circhetta <francesco.circhetta@gmail.com>
2018-12-11 17:14:38 +0100

    :sparkles: Upload first release

---
+++ zerotier-one-rpi0/rootfs/etc/cont-init.d/10-zerotier-one.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/with-contenv bash
+# ==============================================================================
+# Francesco's Hass.io Add-ons: ZeroTier One
+# Take down the S6 supervision tree when ZeroTier One fails
+# ==============================================================================
+source /usr/lib/hassio-addons/base.sh
+
+auth_token=$(hass.config.get 'auth_token')
+echo ${auth_token} > /data/authtoken.secret
+
+network_id=$(hass.config.get 'network_id')
+
+if ! hass.directory_exists "/data/networks.d"; then
+    mkdir /data/networks.d
+fi
+
+touch /data/networks.d/${network_id}.conf
+
+
+ln -s /data /var/lib/zerotier-one
